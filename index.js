9c06e301fccf2037f74346461ecbd63299d68d24
jastasovas <jastasovas@gmail.com>
2023-11-15 21:09:26 +0200

    Added some units

---
+++ index.js
@@ -0,0 +1,106 @@
+//---------------Unit 1-----------------------------------------//
+
+// let yourGrade = parseInt("Enter your grade:");
+
+// if (yourGrade >= 1 && yourGrade <= 3) {
+//   console.log("Neigiamas pazymys!");
+// } else if (yourGrade >= 4 && yourGrade <= 5) {
+//   console.log("Teigiamas, bet reikia pasistengti!");
+// } else if (yourGrade >= 6 && yourGrade <= 7) {
+//   console.log("Vidutiniskai, ar tikrai tau to gana?");
+// } else if (yourGrade >= 8 && yourGrade <= 9) {
+//   console.log("Pagirk save!!");
+// } else if (yourGrade == 10) {
+//   console.log("Super!!!");
+// } else {
+//   console.log("Galbut suklydai ivesdamas pazymi");
+// }
+
+//---------------Unit 2-----------------------------------------//
+
+// let month = prompt("Enter month name in Lithuanian language:");
+
+// switch (month) {
+//   case "Gruodis":
+//   case "Sausis":
+//   case "Vasaris":
+//     console.log("It's winter month");
+//     break;
+//   case "Kovas":
+//   case "Balandis":
+//   case "Geguze":
+//     console.log("It's spring month");
+//     break;
+//   case "Birzelis":
+//   case "Liepa":
+//   case "Rugpjutis":
+//     console.log("It's summer month");
+//     break;
+//   case "Rugsejis":
+//   case "Spalis":
+//   case "Lapkritis":
+//     console.log("It's autumn month");
+//     break;
+//   default:
+//     console.log("Wrong input or written in lower case.");
+// }
+
+//---------------Unit 3-----------------------------------------//
+
+// const time = new Date();
+// let hourNow = time.getHours();
+
+// if (hourNow >= 6 && hourNow <= 11) {
+//   console.log("Labas rytas!");
+// } else if (hourNow >= 11 && hourNow <= 18) {
+//   console.log("Laba diena!");
+// } else if (hourNow >= 18 && hourNow <= 21) {
+//   console.log("Labas vakaras!");
+// } else {
+//   console.log("Naktis juk!!!!");
+// }
+
+//---------------Unit 4-----------------------------------------//
+
+// const date = new Date();
+// const day = date.getDay();
+
+// switch (day) {
+//   case 1:
+//     console.log("Monday");
+//     break;
+//   case 2:
+//     console.log("Tuesday");
+//     break;
+//   case 3:
+//     console.log("Wednesday");
+//     break;
+//   case 4:
+//     console.log("Thursday");
+//     break;
+//   case 5:
+//     console.log("Friday");
+//     break;
+//   case 6:
+//     console.log("Saturday");
+//     break;
+//   case 7:
+//     console.log("Sunday");
+//     break;
+// }
+
+//---------------Unit 5-----------------------------------------//
+
+// let yourAge = parseInt("Enter your age here:");
+
+// if (yourAge <= 18) {
+//   if (yourAge == 18) {
+//     console.log("Pilnametis, gali tureti teises.");
+//   } else {
+//     console.log("Nepilnametis, teisiu negali tureti");
+//   }
+// } else {
+//   console.log("Pilnametis, gali tureti teises");
+// }
+
+//---------------Unit 6-----------------------------------------//
